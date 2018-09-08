4d67ef30613eba5944e8848532c8f3cba9f882b6
NanakiX3 <julinhop72@gmail.com>
2018-09-08 15:38:46 +0200

    Mise à jour de l'interface admin

--- dashboard/layout/layout.php
+++ dashboard/layout/layout.php
@@ -8,6 +8,7 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+        <link rel="stylesheet" href="././web/css/bootstraptheme.min.css">
         <link rel="stylesheet" href="./web/css/style.css">
 
     </head>
